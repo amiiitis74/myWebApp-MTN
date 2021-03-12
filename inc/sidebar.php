caebff23faf918fcce0b9eb86aaf641ea7f5fc1f
sara sh <amiiitis_74@yahoo.com>
2021-03-12 20:16:47 +0330

    Icon update
    Location icon of sidebar updated.

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -4,7 +4,7 @@
                             <a class="nav-link active" href="../index.php"><img class="sidebarImg" src="./img/png24/dashboard-1288244.png" style="width:24px;height:24px;"/>Dashboard</a>
                         </li>
                         <li class="nav-item sidebarItem">
-                            <a class="nav-link" href="../view_reportsOnMap.php"><img class="sidebarImg" src="./img/png24/exclamation-mark-1214332.png" style="width:24px;height:24px;"/>Reports on Map</a>
+                            <a class="nav-link" href="../view_reportsOnMap.php"><img class="sidebarImg" src="./img/png24/location.png" style="width:24px;height:24px;"/>Reports on Map</a>
                         </li>
                         <li class="nav-item sidebarItem">
                             <a class="nav-link" href="../view_filters.php"><img class="sidebarImg" src="./img/png24/repair-tool-1218383.png" style="width:24px;height:24px;"/>Filters and Options</a>
